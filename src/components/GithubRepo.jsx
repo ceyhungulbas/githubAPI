16f827335e3c1a2919426eae1e4b9dd22622c650
CeyhunGulbas <cetree61@gmail.com>
2021-10-07 22:09:04 +0300

    Fix: yarn run fix

--- src/components/GithubRepo.jsx
+++ src/components/GithubRepo.jsx
@@ -1,28 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const GithubRepo = (props) =>{
-    console.log("props: ", props);
+const GithubRepo = (props) => {
+  console.log("props: ", props);
 
-    const [githubProfileRepo, setGithubProfileRepo] = useState([])
+  const [githubProfileRepo, setGithubProfileRepo] = useState([]);
 
-    useEffect(() =>{
-        fetch(`https://api.github.com/users/${props.user.login}/repos`)
-            .then(response => response.json())
-            .then(data => setGithubProfileRepo(data))
-    }, [props.user.login])
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${props.user.login}/repos`)
+      .then((response) => response.json())
+      .then((data) => setGithubProfileRepo(data));
+  }, [props.user.login]);
 
-    return(
-        <>
-            {githubProfileRepo.map((info, key) => {
-                return(
-                    <div className="githubRepo" key={key}>
-                        <a href={info.html_url} target="_blank" rel="noopener noreferrer">- {info.name}</a>
-                    </div>
-                )
-            })}
-        </>
-    )
-
-}
+  return (
+    <>
+      {githubProfileRepo.map((info, key) => {
+        return (
+          <div className="githubRepo" key={key}>
+            <a href={info.html_url} target="_blank" rel="noopener noreferrer">
+              - {info.name}
+            </a>
+          </div>
+        );
+      })}
+    </>
+  );
+};
 
 export default GithubRepo;
--- src/components/githubAPI.jsx
+++ src/components/githubAPI.jsx
@@ -1,64 +1,66 @@
-import React, { useState, useEffect } from 'react';
-import GithubRepo from './GithubRepo';
+import React, { useState, useEffect } from "react";
+import GithubRepo from "./GithubRepo";
 import { Github } from "react-bootstrap-icons";
-import { Button } from 'react-bootstrap';
-import "./GithubAPI.css"
+import { Button } from "react-bootstrap";
+import "./GithubAPI.css";
 
+const GithubAPI = () => {
+  const [user, setUser] = useState("ceyhungulbas");
+  const [tempUser, setTempUser] = useState("");
+  const [githubProfile, setGithubProfile] = useState([]);
 
-const GithubAPI = () =>{
-    const [user, setUser] = useState("ceyhungulbas")
-    const [tempUser, setTempUser] = useState("")
-    const [githubProfile, setGithubProfile] = useState([])
+  useEffect(() => {
+    fetch(`https://api.github.com/search/users?q=${user}`)
+      .then((response) => response.json())
+      .then((data) => setGithubProfile(data.items));
+  }, [user]);
 
-    useEffect(() =>{
-        fetch(`https://api.github.com/search/users?q=${user}`)
-            .then(response => response.json())
-            .then(data => setGithubProfile(data.items))
-    }, [user])
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUser(tempUser);
+  };
 
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setUser(tempUser)
-    }
-
-
-
-    return(
-        <div className="githubAPIMainDiv">
-            <h1>Github API</h1>
-            <div className="githubAPIMainDivFormArea">
-                <form onSubmit={handleSubmit}>
-                    <input 
-                        type="text"
-                        onChange={(e) => setTempUser(e.target.value)}
-                        placeholder="Username"
-                    />
-                    <Button variant="dark" onClick={handleSubmit}>Search!</Button>
-                </form>
-            
-            </div>
-            <div className="github">
-                <div className="githubProfileParentDiv">
-                    {githubProfile.map((user, key) => {
-                    return(
-                        <div className="githubProfileChildDiv" key={key}>
-                            <h2>{user.login}</h2>
-                            <img src={user.avatar_url} alt={user.login} />
-                            <div className="githubProfileChildDivDetails">
-                                <a href={user.html_url} target="_blank" rel="noopener noreferrer"><Github /></a>
-                            </div>
-                            <div className="githubProfileRepoParentDiv">
-                                <GithubRepo user={user} />
-                            </div>
-                        </div>
-                        
-                        )
-                    })}
+  return (
+    <div className="githubAPIMainDiv">
+      <h1>Github API</h1>
+      <div className="githubAPIMainDivFormArea">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            onChange={(e) => setTempUser(e.target.value)}
+            placeholder="Username"
+          />
+          <Button variant="dark" onClick={handleSubmit}>
+            Search!
+          </Button>
+        </form>
+      </div>
+      <div className="github">
+        <div className="githubProfileParentDiv">
+          {githubProfile.map((user, key) => {
+            return (
+              <div className="githubProfileChildDiv" key={key}>
+                <h2>{user.login}</h2>
+                <img src={user.avatar_url} alt={user.login} />
+                <div className="githubProfileChildDivDetails">
+                  <a
+                    href={user.html_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Github />
+                  </a>
                 </div>
-            </div>
+                <div className="githubProfileRepoParentDiv">
+                  <GithubRepo user={user} />
+                </div>
+              </div>
+            );
+          })}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default GithubAPI
+export default GithubAPI;
